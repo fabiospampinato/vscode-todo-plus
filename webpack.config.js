768d8983a269274dad852f4192e95f5bd2dc597f
Fabio Spampinato <spampinabio@gmail.com>
2018-10-10 17:16:33 +0200

    Bundling with webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+/* IMPORT */
+
+const path = require ( 'path' );
+
+/* CONFIG */
+
+const config = {
+  target: 'node',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve ( __dirname, 'out' ),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode',
+    fsevents: 'commonjs vscode'
+  },
+  resolve: {
+    extensions: ['tsx', '.ts', '.jsx', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'ts-loader'
+        }
+      ]
+    }]
+  }
+}
+
+/* EXPORT */
+
+module.exports = config;
