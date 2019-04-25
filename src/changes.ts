aad446dc0a6396be8356666834e57f52850dceae
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-04-25 11:16:43 -0700

    Added the changes.ts file to look when an enter is inputted to add/remove todos

---
+++ src/changes.ts
@@ -0,0 +1,70 @@
+
+/* IMPORT */
+
+import * as _ from 'lodash';
+import * as vscode from 'vscode';
+import Consts from './consts';
+import { toggleBox } from './commands'
+import Document from './todo/document'
+
+
+const Editor = {
+
+  textEditor: vscode.window.activeTextEditor
+
+}
+
+/* CHANGES */
+
+const Changes = {
+
+  textEditor: vscode.window.activeTextEditor,
+
+  doc: new Document ( Editor.textEditor ),
+
+  changes: [],
+
+  onChanges ({ document, contentChanges }) {
+
+    if ( document.languageId !== Consts.languageId ) return;
+
+    if ( !contentChanges.length ) return; //URL: https://github.com/Microsoft/vscode/issues/50344
+
+    Changes.changes.push ( ...contentChanges );
+
+    Changes.symbolInsertion ( document );
+
+  },
+
+  symbolInsertion ( document: vscode.TextDocument ) {
+    //get the text of the last change
+    const lastChange = Changes.changes[ String( Changes.changes.length - 1 ) ],
+          currentLine = lastChange.range._end.line,
+          lastChangeText = lastChange.text;
+
+    //an enter is inputed
+    if ( lastChangeText.match(/[\s\t]*\n[\s\t]*/) ) {
+
+        //still working on this
+        const todo = Changes.doc.getTodoAt(currentLine),
+              todoText = todo.text.replace(/\u2610/,'');
+
+        //last line was an empty todo line
+        if (todoText.match(/^[\s\t]*$/)){
+          //remove the empty todo
+          toggleBox();
+
+        }
+        //last line was a filled todo line
+        else if(todoText && !todoText.match(/^[\s\t]*$/)){
+          //here we add the toggleBox but in the next line
+        }
+    }
+    
+  }
+
+};
+
+/* EXPORT */
+
+export default Changes;
