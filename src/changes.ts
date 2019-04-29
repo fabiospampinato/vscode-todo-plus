be9e6c305c38fdf68747d5931e6025bf197164c0
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-04-29 15:11:57 -0700

    Added new command and minor refactor and comments changes

--- src/changes.ts
+++ src/changes.ts
@@ -4,7 +4,7 @@
 import * as _ from 'lodash';
 import * as vscode from 'vscode';
 import Consts from './consts';
-import { toggleBox } from './commands'
+import { toggleBox, toggleBoxChain } from './commands'
 import Document from './todo/document'
 
 
@@ -32,32 +32,32 @@ const Changes = {
 
     Changes.changes.push ( ...contentChanges );
 
-    Changes.symbolInsertion ( document );
+    Changes.symbolInsertion ();
 
   },
 
-  symbolInsertion ( document: vscode.TextDocument ) {
-    //get the text of the last change
+  symbolInsertion () {
+    //Get the text of the last change
     const lastChange = Changes.changes[ String( Changes.changes.length - 1 ) ],
           currentLine = lastChange.range._end.line,
           lastChangeText = lastChange.text;
 
-    //an enter is inputed
+    //An enter is inputed
     if ( lastChangeText.match(/[\s]*\n[\s]*/) ) {
 
-        //still working on this
         const todo = Changes.doc.getTodoAt(currentLine),
               todoText = todo.text.replace(Consts.regexes.todoSymbol,'');
 
-        //last line was an empty todo line
+        //Last line was an empty todo line
         if (todoText.match(/^[\s]*$/)){
-          //remove the empty todo
+          //Remove the empty todo
           toggleBox();
 
         }
-        //last line was a filled todo line
+        //Last line was a filled todo line
         else if(todoText && !todoText.match(/^[\s]*$/)){
-          //here we add the toggleBox but in the next line
+          //Add an empty todo
+          toggleBoxChain();
 
         }
     }
