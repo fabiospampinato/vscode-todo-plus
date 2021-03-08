0e5cd07511fd1b8bb57f5938ed92f0613ebc8f1b
Fabio Spampinato <spampinabio@gmail.com>
2021-03-08 18:21:31 +0000

    Ensuring ts-loader doesn’t prevent production builds to be made

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,10 @@ const config = {
       exclude: /node_modules/,
       use: [
         {
-          loader: 'ts-loader'
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
         }
       ]
     }]
